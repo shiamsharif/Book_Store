f245c268c0bd6f993323b1a8e7e9b7dfd3b3c619
shiamsharif <shiam.sharif.07@gmail.com>
2025-05-01 20:49:40 +0600

    ADD: comment

--- cart/forms.py
+++ cart/forms.py
@@ -17,3 +17,21 @@ class CartAddProductForm(forms.Form):
         initial=False,
         widget=forms.HiddenInput
     )
+
+
+# from django import forms
+
+# PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+
+
+# class CartAddProductForm(forms.Form):
+#     quantity = forms.TypedChoiceField(
+#         choices=PRODUCT_QUANTITY_CHOICES,
+#         coerce=int
+#     )
+#     override = forms.BooleanField(
+#         required=False,
+#         initial=False,
+#         widget=forms.HiddenInput
+#     )
+
